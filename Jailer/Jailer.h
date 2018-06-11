3791c2abb2d6d4bdfffcb47cfc3ccafb8b7f5e73
Shota Shimazu <hornet.live.mf@gmail.com>
2018-06-12 01:41:07 +0900

    Initial Commit

---
+++ Jailer/Jailer.h
@@ -0,0 +1,19 @@
+//
+//  Jailer.h
+//  Jailer
+//
+//  Created by Shota Shimazu on 2018/06/12.
+//  Copyright © 2018 MiXSTAGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jailer.
+FOUNDATION_EXPORT double JailerVersionNumber;
+
+//! Project version string for Jailer.
+FOUNDATION_EXPORT const unsigned char JailerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jailer/PublicHeader.h>
+
+
